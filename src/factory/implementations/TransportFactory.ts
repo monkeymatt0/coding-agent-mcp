f56fa0df173c9325ed84d7387ec2615ef801123a
Matteo Vollero <m.vollero96@gmail.com>
2026-03-10 21:13:22 +0100

    feature(HTTPTransport): create type and interface

---
+++ src/factory/implementations/TransportFactory.ts
@@ -0,0 +1,15 @@
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio";
+import { TransportInterface } from "../interfaces/TransportInterface";
+import { Transport } from "../types/Transport";
+import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp";
+
+export class TransportFactory implements TransportInterface {
+     createTransport(type: string): Transport {
+          switch(type){
+               case 'http':
+                    return new StreamableHTTPServerTransport();
+               default:
+                    return new StdioServerTransport();
+          }
+     }
+}
